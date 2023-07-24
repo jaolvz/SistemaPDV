0f36fc2cab081748f21ee6a6b4a390775bf59932
João Vitor <joaoloveze@outlook.com>
2023-07-24 06:44:05 -0300

    Finalizada toda parte visual da tela de carregamento, tela de venda e tela de inicio.

--- main.py
+++ main.py
@@ -6,16 +6,29 @@ from kivy.lang import Builder
 from kivy.clock import Clock
 
 
+class TelaVenda(Screen):
+    pass
+
+
+
+class TelaInicial(Screen):
+    def on_enter(self):
+        Window.bind(on_keyboard=self.tecla_pressionada)
+    def tecla_pressionada(self,*args):
+        self.manager.current = "tela_venda"
 
 class TelaCarregamento(Screen):
     def on_enter(self):
         Clock.schedule_once(self.efeitoFade)
 
     def efeitoFade(self,dt):
-        animacao = Animation(opacity=1, duration=2)
+        animacao = Animation(opacity=1, duration=4 )
+        animacao.bind(on_complete=self.change_screen)
         animacao.start(self.ids.logo_carregamento)
-        #animacao.bind(on_complete=self.do_something_after_fade)
 
+
+    def change_screen(self,*args):
+        self.manager.current = "tela_inicial"
 
 
 class gerenciador_Telas(ScreenManager):
